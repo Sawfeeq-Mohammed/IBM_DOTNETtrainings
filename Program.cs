95dfb97330e60761bdd9fee30be31c605d449163
Sawfeeq-Mohammed <100554710+Sawfeeq-Mohammed@users.noreply.github.com>
2022-03-01 11:19:26 +0530

    Day 2 Task 1
    Basics of C# programming

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyirstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            String name = "Pan";
+            String company = "IBM";
+
+            Console.WriteLine("My name is {0} and i work at {1} ",name,company);
+
+            int c = 4 + 7;
+            Console.WriteLine(c);
+
+            double d = 1.0798;
+            bool t = true;
+            char a = 'A';
+            Console.WriteLine(d);
+            Console.WriteLine(t);
+            Console.WriteLine(a);
+            Console.WriteLine(" ");
+
+            //Type Cating
+            double d1 = 9.001;
+            int l2 = (int)d1;
+            Console.WriteLine(l2);
+            Console.WriteLine(" ");
+
+
+            int num1, num2,num3;
+
+            Console.WriteLine("Enter num1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num2");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter num3");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3) { 
+                    Console.WriteLine("A is greatest");
+                }
+            else if(num2>num1 && num2 >num3)
+            {
+                Console.WriteLine("B is greatest");
+
+            }
+            else
+            {
+                Console.WriteLine("C is greatest");
+            }
+            Console.WriteLine(" ");
+
+            int a1, b1;
+            Console.WriteLine("Enter a1 ");
+            a1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b1");
+            b1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before swapping:" + a1 +" "+ b1);
+            a1 = a1 + b1;
+            b1 = a1 - b1;
+            a1 = a1 - b1;
+
+            Console.WriteLine("The swapped Variables are:" + a1 +" "+ b1);
+            Console.WriteLine(" ");
+
+            String name1, address;
+            int age, mob,empid;
+
+            Console.WriteLine("Enter Employee name:");
+            name1 = Console.ReadLine();
+            Console.WriteLine("Enter Employee ID:");
+            empid = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee age:");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Address:");
+            address = Console.ReadLine();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Name:" + name1);
+            Console.WriteLine("Employee ID:" + empid);
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("Address:" + address);
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
